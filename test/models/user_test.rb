0f1293c9b97ba49676de528dfdf86690556597f1
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-04-10 23:54:36 -0400

    Test case for email downcase (solves exercise 6.1)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -52,8 +52,15 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExaMPle.coM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "password should have minimum length" do
-    @user.password = @user.password_confirmation =  "a" * 5
+    @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
 end
