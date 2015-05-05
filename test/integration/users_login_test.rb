e281dbadc710ede57f3381c83c44a07fadbb8ded
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-05-04 22:22:15 -0400

    Fix failing test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,9 +18,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information" do
     get login_path
-    post login_path, session: {email: @user.email, password: ""}
+    post login_path, session: { email: @user.email, password: 'password' }
     assert_redirected_to @user
-    follow.redirect!
+    follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
