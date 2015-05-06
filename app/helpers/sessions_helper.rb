ace098b12af362e94928eee8745a43ebd96d6957
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-05-06 19:36:19 -0400

    Logging out
    Using session helper to destroy the user and making current user nil

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,6 @@
 module SessionsHelper
 
+  # Logs in current user
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -10,5 +11,11 @@ module SessionsHelper
 
   def logged_in?
     !current_user.nil?
+  end
+
+  # Logs out current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
