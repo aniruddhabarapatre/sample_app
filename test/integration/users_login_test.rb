74d227ccff1630e8a52e479fd96b8a0fe2bce0de
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-05-13 19:00:24 -0400

    Friendly Forwarding

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -8,9 +8,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with invalid information" do
     get login_path
-    assert_template "sessions/new"
+    assert_template 'sessions/new'
     post login_path, session: { email: "", password: "" }
-    assert_template "sessions/new"
+    assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
@@ -29,11 +29,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
-    # Simulate a user clicking logout in second window
+    # Simulate a user clicking logout in a second window.
     delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 
