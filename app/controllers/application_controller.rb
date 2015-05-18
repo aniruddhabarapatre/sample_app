fc27a0c41e5d44887b804d8b4dfbac8fb758799b
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-05-17 23:27:56 -0400

    User destroy tests
    Controller and integration test for destroy action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  # Stackoverflow suggestion to pass test on chapter 9
+  # WillPaginate.per_page = 10
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def new
     @user = User.new
@@ -68,6 +69,6 @@ class UsersController < ApplicationController
 
     # Confirms an admin user
     def admin_user
-      redirect_to root_url unless current_user.admin?
+      redirect_to(root_url) unless current_user.admin?
     end
 end
